0b46609f6bcf3f18626a01dd3096ebd43d2309ed
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-02-18 23:05:08 +0100

    5.	Vytvořte třídu AppUser (entitu) s atributy podle databázového modelu níže. 6.	Přidejte do application.yaml následující vlastnost a restartujte Spring Boot aplikaci: spring.jpa.hibernate.ddl-auto=create

---
+++ src/main/java/cz/upce/fe/cv02/domain/AppUser.java
@@ -0,0 +1,32 @@
+package cz.upce.fe.cv02.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class AppUser {
+    @Id
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private Boolean active;
+
+    @Column
+    private LocalDateTime creationDate;
+
+    @Column
+    private LocalDateTime updateDate;
+}

79706c463cb7dc987039f68db4e7711c9bb3afa0
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-02-27 14:07:55 +0100

    Provizorní oprava chyby která způsobovala rekurzivní chování při použití endpointu
    Standartizace adresy endpointu.

--- src/main/java/cz/upce/fe/cv02/controller/UserController.java
+++ src/main/java/cz/upce/fe/cv02/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/appUser")
+@RequestMapping("/app-user")
 public class UserController {
     private final AppUserRepository appUserRepository;
 
